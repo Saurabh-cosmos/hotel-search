67daba78084f4e67689bd3ad022d03716be59586
Saurabh-cosmos <saurabh33k1@gmail.com>
2025-02-12 10:23:29 +0530

    added pagination

--- src/app/ui-pages/hotel-list/hotel-list.component.ts
+++ src/app/ui-pages/hotel-list/hotel-list.component.ts
@@ -1,24 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-
 declare var bootstrap: any;
-
 
 @Component({
   selector: 'app-hotel-list',
   templateUrl: './hotel-list.component.html',
   styleUrls: ['./hotel-list.component.scss']
 })
-export class HotelListComponent {
+export class HotelListComponent implements OnInit, OnDestroy {
   hotelData: any;
   selectedHotel: any;
+  hotelsToShow: any[] = [];
+  hotelsPerPage = 10;
+  isLoading: boolean = true;
+  isLoadingMore: boolean = false;
+
+  @ViewChild('loadMore', { static: false }) loadMore!: ElementRef;
 
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.hotelData = history.state.data.message;
-    console.log(this.hotelData);
+    setTimeout(() => {
+      this.hotelsToShow = this.hotelData.hotels.slice(0, this.hotelsPerPage);
+      this.isLoading = false;
+    }, 2000); // Simulate a 2-second delay
+  }
+
+  loadMoreHotels(): void {
+    this.isLoadingMore = true;
+    setTimeout(() => {
+      const currentLength = this.hotelsToShow.length;
+      const moreHotels = this.hotelData.hotels.slice(currentLength, currentLength + this.hotelsPerPage);
+      this.hotelsToShow = [...this.hotelsToShow, ...moreHotels];
+      this.isLoadingMore = false;
+      if (this.hotelsToShow.length === this.hotelData.hotels.length) {
+        this.loadMore.nativeElement.style.display = 'none';
+      }
+    }, 2000); // Simulate a 2-second delay before loading more data
   }
 
   openModal(hotel: any): void {
@@ -26,5 +46,9 @@ export class HotelListComponent {
     const modalElement = document.getElementById('hotelDetailsModal');
     const modal = new bootstrap.Modal(modalElement);
     modal.show();
+  }
+
+  ngOnDestroy(): void {
+    // Clean up if necessary
   }
 }
